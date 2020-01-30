70ae099399eaa6de5175dbf914a9ca57a25f4571
isaaccolson <isaac.colson@gmail.com>
2020-01-30 12:02:56 -0500

    Second Upload

---
+++ Models/Scrape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockScraper.Models
+{
+    public class Scrape
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Change { get; set; }
+        public string Volume { get; set; }
+        public string DayRange { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+    }
+}
---
+++ Models/ScrapeDateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockScraper.Models
+{
+    public class ScrapeDateViewModel
+    {
+        public List<Scrape> Scrapes { get; set; }
+        public SelectList Dates { get; set; }
+        public string Date { get; set; }
+    }
+}
